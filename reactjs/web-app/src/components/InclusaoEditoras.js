0bc6d875660048b973ff8f802b5d9c9cf6929501
dacasfilipe <filiperdacas@gmail.com>
2023-04-22 09:37:13 -0300

    adicionado o arquivo InclusaoEditoras.js com o frontend para a inserção de novas editoras no banco utilizando o react e axios

---
+++ reactjs/web-app/src/components/InclusaoEditoras.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { inAxios } from "../config_axios";
+
+const InclusaoEditoras = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const [aviso, setAviso] = useState("");
+
+  const salvar = async (campos) => {
+    try {
+      const response = await inAxios.post("editoras", campos);
+      setAviso(`Ok! Editora cadastrada com código ${response.data.id}`);
+      limparFormulario();
+    } catch (error) {
+      setAviso(`Erro...editora não cadastrada: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAviso("");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [aviso]);
+
+  const limparFormulario = () => {
+    reset({
+      nome: "",
+    });
+  };
+
+  return (
+    <div className="container">
+      <h4 className="fst-italic mt-3">Inclusão de Editoras</h4>
+      <form onSubmit={handleSubmit(salvar)}>
+        <div className="form-group">
+          <label htmlFor="nome">Nome:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="nome"
+            required
+            autoFocus
+            {...register("nome")}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary mt-3" value="Enviar" />
+        <input
+          type="reset"
+          className="btn btn-danger mt-3 ms-3"
+          value="Limpar"
+        />
+      </form>
+      <div
+        className={
+          aviso.startsWith("Ok!")
+            ? "alert alert-sucess"
+            : aviso.startsWith("Erro:")
+            ? "alert alert-danger"
+            : ""
+        }
+      >
+        {aviso}
+      </div>
+    </div>
+  );
+};
+
+export default InclusaoEditoras;
