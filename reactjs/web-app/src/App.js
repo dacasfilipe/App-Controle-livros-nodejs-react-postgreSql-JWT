7b8945868dfb055e83e60d579820945e965702c7
dacasfilipe <filiperdacas@gmail.com>
2023-04-23 08:30:41 -0300

    atualizado app.js com a inclusão das rotas para adicionar autores e editoras.

--- reactjs/web-app/src/App.js
+++ reactjs/web-app/src/App.js
@@ -1,6 +1,8 @@
 import {Routes, Route} from "react-router-dom";
 import MenuSuperior from "./components/MenuSuperior";
 import InclusaoLivros from "./components/InclusaoLivros";
+import InclusaoAutores from "./components/InclusaoAutores";
+import InclusaoEditoras from "./components/InclusaoEditoras";
 import ManutencaoLivros from "./components/ManutencaoLivros";
 import ResumoLivros from "./components/ResumoLivros";
 
@@ -10,7 +12,9 @@ const App = () => {
     <>  
       <MenuSuperior />
       <Routes>
-        <Route path="/" element={<InclusaoLivros />} />
+      <Route path="/" element={<InclusaoLivros />} />
+        <Route path="/inclusao-autores" element={<InclusaoAutores />} />
+        <Route path="/inclusao-editoras" element={<InclusaoEditoras />} />
         <Route path="/manut" element={<ManutencaoLivros />} />
         <Route path="/resumo" element={<ResumoLivros />} />
       </Routes>
