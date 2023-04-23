ec1ca0be311f0cf274dc1a77342c54a7341399c6
dacasfilipe <filiperdacas@gmail.com>
2023-04-23 08:25:30 -0300

    atualizado o component react menuSuperior.js

--- reactjs/web-app/src/components/MenuSuperior.js
+++ reactjs/web-app/src/components/MenuSuperior.js
@@ -6,16 +6,32 @@ const MenuSuperior = () => {
             <div className="container">
                 <Link to="/" className="navbar-brand" >Controle Pessoal de Livros</Link>
                 <ul className="navbar-nav">
-                    <li>
-                        <Link to="/" className="nav-link">Inclusão</Link>
-                    </li>
-                    <li>
-                        <Link to="/manut" className="nav-link">Manutenção</Link>
-                    </li>
-                    <li>
-                        <Link to="/resumo" className="nav-link">Resumo</Link>
-                    </li>
-                </ul>
+          <li>
+            <Link to="/" className="nav-link">
+              Inclusão Livros
+            </Link>
+          </li>
+          <li>
+            <Link to="/inclusao-autores" className="nav-link">
+              Inclusão Autores
+            </Link>
+          </li>
+          <li>
+            <Link to="/inclusao-editoras" className="nav-link">
+              Inclusão Editoras
+            </Link>
+          </li>
+          <li>
+            <Link to="/manut" className="nav-link">
+              Manutenção
+            </Link>
+          </li>
+          <li>
+            <Link to="/resumo" className="nav-link">
+              Resumo
+            </Link>
+          </li>
+        </ul>
             </div>
         </nav>
     );
