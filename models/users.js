ccf9d5f8bfed220fd28735a94fbbd66648b2599e
dacasfilipe <filiperdacas@gmail.com>
2023-05-03 23:51:33 -0300

    refatorado user.js

--- models/users.js
+++ models/users.js
@@ -1,20 +1,20 @@
-const	bcrypt	=	require('bcrypt');
-const	{	DataTypes	}	=	require('sequelize');
-module.exports	=	(app)	=>	{
-		const	Users	=	app.db.define('Users',	{
-				//	Os	demais	campos	desse	modelo	foram	criados	no	capítulo	5
-				password:	{
-						type:	DataTypes.STRING,
-						allowNull:	false,
-						validate:	{
-								notEmpty:	true
+const bcrypt = require('bcrypt');
+const { DataTypes }	= require('sequelize');
+module.exports = (app) => {
+		const Users = app.db.define('Users', {
+				//	Os demais campos desse modelo foram	criados	no capítulo	5
+				password: {
+						type: DataTypes.STRING,
+						allowNull: false,
+						validate: {
+							notEmpty: true
 						},
-						set(value)	{
-								const	salt	=	bcrypt.genSaltSync();
-								const	password	=	bcrypt.hashSync(value,	salt);
-								this.setDataValue('password',	password);
+						set(value){
+								const salt = bcrypt.genSaltSync();
+								const password = bcrypt.hashSync(value,	salt);
+								this.setDataValue('password', password);
 						}
 				}
 		});
-		return	Users;
+		return Users;
 };
