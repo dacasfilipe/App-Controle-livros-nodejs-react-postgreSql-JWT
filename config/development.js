8aae4b54cf05b1561963046afe6ea4ab8d8f9457
dacasfilipe <filiperdacas@gmail.com>
2023-05-10 23:52:18 -0300

    inserido comentários no código para documentação

--- config/development.js
+++ config/development.js
@@ -1,15 +1,18 @@
 module.exports = {
+    // Configuração do banco de dados.
     db:{
-        database: 'livros',
+        database: 'livros',//nome do banco de dados
         username: 'postgres',
         password: 'postgres',
         params: {
-            dialect: 'postgres',
+            dialect: 'postgres',//dialeto do banco
             define: {
-                underscored: true
+                // Configuração para usar underscores (em vez de camelCase) nos nomes das tabelas e colunas.
+                underscored: true 
             }
         }
     },
+    // Configuração do JWT (JSON Web Tokens) para autenticação
     jwt: {
         secret: 'SECRET_TOKEN',
         options: { session: false }
