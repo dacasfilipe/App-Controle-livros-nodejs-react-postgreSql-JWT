31b52d6a2c94643fdda418f549c044d32d220054
dacasfilipe <filiperdacas@gmail.com>
2023-05-11 22:41:50 -0300

    comentado o código do index.js

--- reactjs/web-app/src/index.js
+++ reactjs/web-app/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import App from './App';
+// Importa os módulos necessários para a apllicação
+import React from 'react'; //biblioteca react
+import ReactDOM from 'react-dom/client';// Biblioteca ReactDOM para renderização no cliente
+import {BrowserRouter} from "react-router-dom";// Componente BrowserRouter do react-router-dom para roteamento
+import App from './App';// Componente principal da aplicação
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Cria uma raiz para a renderização do React usando a div com id 'root' no HTML
+const root = ReactDOM.createRoot(document.getElementById('root')); // Renderiza o componente principal da aplicação (App) dentro de um componente BrowserRouter
 root.render(
+  // BrowserRouter é utilizado para habilitar o roteamento na aplicação
+  // App é o componente principal da aplicação
   <BrowserRouter>
-    <App />
+    <App /> 
   </BrowserRouter>
 );
